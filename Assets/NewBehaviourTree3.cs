4f433bc7c2f5d8dc44844eca79cd15cbc41a3ec5
Christine2017 <lu.mingxiao27@gmail.com>
2017-11-05 22:50:29 -0500

    yugugtg dsd

---
+++ Assets/NewBehaviourTree3.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using TreeSharpPlus;
+
+public class NewBehaviourTree3 : MonoBehaviour
+{
+    public Transform A0;
+    public Transform tablepoint;
+    public Transform B0;
+    public Transform meetpoint;
+    public Transform table;
+    public Transform wander1;
+    public Transform wander2;
+    public Transform wander3;
+    public Transform thrower;
+    public Transform wall;
+    public GameObject ball;
+    public GameObject peopleA;
+    public GameObject peopleB;
+    public GameObject peopleC;
+    private BehaviorAgent behaviorAgent;
+
+    // Use this for initialization
+    void Start()
+    {
+        behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
+        BehaviorManager.Instance.Register(behaviorAgent);
+        behaviorAgent.StartBehavior();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected Node move( GameObject people, Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(people.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+    }
+
+    protected Node moveToBall(GameObject people, GameObject ball)
+    {
+        Val<Vector3> position = Val.V(() => ball.transform.position);
+        return new Sequence(people.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+    }
+
+    protected Node BuildTreeRoot()
+    {
+        Node roaming = new DecoratorLoop
+                        (
+                        new Sequence
+                        (
+                            new Sequence
+                            (
+                                this.move(peopleA, this.tablepoint),
+                                this.catchBall(peopleA, ball),
+                                this.move(peopleA, this.A0),
+                                new Sequence
+                                (
+                                    this.putBall(peopleA, ball, this.thrower),
+                                    new LeafWait(1000),
+                                    this.throwBall(ball)
+                                 )
+                             ),
+                            this.hitGround(ball),
+                            new Selector
+                            (
+                                this.outOfYard(ball),
+                                new SelectorParallel
+                                (
+                                    new DecoratorLoop
+                                    (
+                                        new SequenceShuffle
+                                        (
+                                             this.move(peopleA, this.wander1),
+                                             this.move(peopleA, this.wander2),
+                                             this.move(peopleA, this.wander3)
+                                        )
+                                    ),
+                                    new Sequence
+                                    (
+                                        this.move(peopleB, this.meetpoint),
+                                        new SequenceParallel
+                                        (
+                                            this.sayHi(peopleB, peopleC),
+                                            this.sayHi(peopleC, peopleB)
+                                        )
+                                    )
+                               )
+                            ),
+                            new Sequence
+                            (
+                                this.moveToBall(peopleB, ball),
+                                this.squat(peopleB),
+                                this.catchBall(peopleB, ball),
+                                this.move(peopleB, tablepoint),
+                                this.putBall(peopleB, ball, this.table),
+                                this.move(peopleB, this.B0)
+                            )
+                        )
+                    );
+        return roaming;
+    }
+}
---
+++ Assets/emit_ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class emit_ball : MonoBehaviour {
+
+    public Rigidbody ball;
+    public GameObject wall;
+    public GameObject wall2;
+    //private Vector3 w;
+    // Use this for initialization
+	void Start () {
+        Vector3 w = new Vector3(-50 * Time.deltaTime, 10 * Time.deltaTime, -50 * Time.deltaTime);
+        ball.AddForce(w * 50);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector3 w = new Vector3(-50 * Time.deltaTime, 10 * Time.deltaTime, -50 * Time.deltaTime);
+        //ball.AddForce(w * 20);
+        if (Input.GetKey(KeyCode.Space)) {
+            wall.SetActive(false);
+            wall2.SetActive(false);
+        }
+	}
+}
