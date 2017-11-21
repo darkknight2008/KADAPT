7986380c0a7e9eb4a423f0fde99376cda207a5a5
Sijian Tang <st509@math.rutgers.edu>
2017-11-21 15:10:01 -0500

    map

---
+++ Assets/PlayerController2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    Animator anim;
+
+    public float speed;
+    public float backspeed;
+    public float angleSpeed;
+    public float jumpHeight;
+    public float extraPower;
+
+    private float velocity;
+    private float angleVelocity;
+    private float distToGround;
+    private bool isOnGround;
+    private Vector3 deltaP;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotate = Input.GetAxis("Horizontal");
+        float move = Input.GetAxis("Vertical");
+
+        Vector3 direction = transform.rotation * new Vector3(0.0f, 0.0f, 1.0f);
+
+        // Move by Position
+        velocity = (move >= 0) ? speed: backspeed;
+        velocity *= move;
+        velocity = (Input.GetKey(KeyCode.Z)) ? velocity * extraPower : velocity;
+        transform.position += velocity * Time.deltaTime * direction;
+        angleVelocity = (move >= 0) ? -angleSpeed : angleSpeed;
+        angleVelocity *= rotate;
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, angleVelocity * Time.deltaTime, 0.0f)) * transform.rotation;
+
+        anim.SetFloat("Speed", velocity);
+    }
+}
---
+++ Assets/camera.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public GameObject player;
+
+    public float rotationSpeed;
+    public float scaleSpeed;
+    public float backSpeed;
+    public float pv;
+
+    private Vector3 initCameraPosition;
+    private Quaternion initCameraRotation;
+    private Vector3 initPlayerPosition;
+    private Quaternion initPlayerRotation;
+    private Quaternion R0t;
+    private Quaternion Rscript;
+    private float cameraDistanceScale;
+    private Vector3 camerPositionLocal;
+    private Quaternion Q;
+
+    // Use this for initialization
+    void Start()
+    {
+        initCameraPosition = transform.position;
+        initCameraRotation = transform.rotation;
+        //player = GameObject.FindGameObjectWithTag("Player");
+        initPlayerPosition = player.transform.position;
+        initPlayerRotation = player.transform.rotation;
+        Rscript = Quaternion.identity;
+        cameraDistanceScale = 1.0f;
+        camerPositionLocal = initCameraPosition - initPlayerPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraDistanceScale += - Input.GetAxis("Mouse ScrollWheel")* Time.deltaTime * scaleSpeed;
+
+        if (Input.GetButton("Fire1"))
+        {
+            camerPositionLocal = Quaternion.AngleAxis(rotationSpeed * Time.deltaTime * Input.GetAxis("Mouse X"), Vector3.up) * camerPositionLocal;
+            if ((Input.GetAxis("Mouse Y") < 0 && camerPositionLocal.y <= 0.95 * camerPositionLocal.magnitude) || (Input.GetAxis("Mouse Y") > 0 && camerPositionLocal.y >= -0.5 * camerPositionLocal.magnitude))
+            {
+                camerPositionLocal = Quaternion.AngleAxis(rotationSpeed * Time.deltaTime * Input.GetAxis("Mouse Y"), Vector3.Cross(Vector3.up, camerPositionLocal)) * camerPositionLocal;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            camerPositionLocal = Quaternion.AngleAxis(-rotationSpeed * Time.deltaTime, Vector3.up) * camerPositionLocal;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            camerPositionLocal = Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, Vector3.up) * camerPositionLocal;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && camerPositionLocal.y <= 0.95 * camerPositionLocal.magnitude)
+        {
+            camerPositionLocal = Quaternion.AngleAxis(-rotationSpeed * Time.deltaTime, Vector3.Cross(Vector3.up, camerPositionLocal)) * camerPositionLocal;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && camerPositionLocal.y >= -0.5 * camerPositionLocal.magnitude)
+        {
+            camerPositionLocal = Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, Vector3.Cross(Vector3.up, camerPositionLocal)) * camerPositionLocal;
+        }
+        if (Input.GetKey("r"))
+        {
+            camerPositionLocal = initCameraPosition - initPlayerPosition;
+            cameraDistanceScale = 1.0f;
+        }
+
+        Q.SetFromToRotation(camerPositionLocal, initCameraPosition - initPlayerPosition);
+
+        pv = Vector3.ProjectOnPlane(player.GetComponent<Rigidbody>().velocity, Vector3.up).magnitude;
+        camerPositionLocal = Quaternion.Lerp(Quaternion.identity, Q, Time.deltaTime * pv * backSpeed) * camerPositionLocal;
+
+        R0t = player.transform.rotation * Quaternion.Inverse(initPlayerRotation);
+        transform.position = player.transform.position + R0t * camerPositionLocal * cameraDistanceScale;
+        Q.SetLookRotation(player.transform.position - transform.position);
+        transform.rotation = Q;
+        //transform.rotation = R0t * Rscript * initCameraRotation;
+    }
+}
