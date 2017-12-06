6f5e5c94f7d5e43e5850108b4dfe4f785d17996a
Sijian Tang <st509@math.rutgers.edu>
2017-12-05 23:17:17 -0500

    character

--- Assets/SwordController.cs
+++ Assets/SwordController.cs
@@ -24,9 +24,13 @@ public class SwordController : MonoBehaviour
         kill = false;
         count = 0.0f;
 
-        transform.position = holdBy.transform.Find("Daniel/UMA_Male_Rig/Global/Position/Hips/LowerBack/Spine/Spine1/RightShoulder/RightArm/RightForeArm/RightHand/SNAP_WalletHold").transform.position;
-        transform.rotation = holdBy.transform.Find("Daniel/UMA_Male_Rig/Global/Position/Hips/LowerBack/Spine/Spine1/RightShoulder/RightArm/RightForeArm/RightHand/SNAP_WalletHold").transform.rotation;
-        transform.parent = holdBy.transform.Find("Daniel/UMA_Male_Rig/Global/Position/Hips/LowerBack/Spine/Spine1/RightShoulder/RightArm/RightForeArm/RightHand/SNAP_WalletHold").transform;
+        //transform.position = holdBy.transform.Find("Daniel/UMA_Male_Rig/Global/Position/Hips/LowerBack/Spine/Spine1/RightShoulder/RightArm/RightForeArm/RightHand/SNAP_WalletHold").transform.position;
+        //transform.rotation = holdBy.transform.Find("Daniel/UMA_Male_Rig/Global/Position/Hips/LowerBack/Spine/Spine1/RightShoulder/RightArm/RightForeArm/RightHand/SNAP_WalletHold").transform.rotation;
+
+        transform.position = holdBy.transform.Find("Hero/Hips/Spine/Spine1/Spine2/RightShoulder/RightArm/RightForeArm/RightHand/Sword").transform.position;
+        transform.rotation = holdBy.transform.Find("Hero/Hips/Spine/Spine1/Spine2/RightShoulder/RightArm/RightForeArm/RightHand/Sword").transform.rotation;
+
+        transform.parent = holdBy.transform.Find("Hero/Hips/Spine/Spine1/Spine2/RightShoulder/RightArm/RightForeArm/RightHand").transform;
     }
 
     // Update is called once per frame
